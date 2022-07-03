695ffd0bd7f1996707a8615d6f43b918ad383ab4
zelfroster <sumitkumarsoni123@gmail.com>
2022-07-03 07:31:56 +0530

    added files

---
+++ Web/SimpleProjects/Calculator/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+        colors: {
+            veryDarkBlue: "#0f0f1f",
+            darkBlue: "#1f1f2f22",
+            blue: "#1363DF22",
+            skyBlue: "#87B5FF22",
+            lightBlue: "#DFF6FF"
+        }
+    },
+  },
+  plugins: [],
+}
